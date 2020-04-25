5f81ee4d6bf7b705ab6660a3804750fdb4272d92
alexiliev <iliev.alex@outlook.com>
2020-04-25 14:06:18 -0700

    adding google maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 console.log('testing')
+var mykey = config.MY_KEY;
+
+function initMap () {
+  // The location of Uluru
+  var SJC = { lat: 37.3353, lng: -121.8919 }
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: SJC
+  })
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: SJC, map: map })
+}
